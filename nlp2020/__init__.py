24d346cb0bbb3574c96e19920550473e42c95e07
MichelangeloConserva <michelangelo.conserva@protonmail.com>
2020-02-12 14:57:05 +0000

    Base setup of environment

--- nlp2020/__init__.py
+++ nlp2020/__init__.py
@@ -1,1 +1,11 @@
+from gym.envs.registration import register
 
+register(
+    id='BaseDungeon',
+    entry_point='nlp2020.envs:BaseDungeon',
+)
+
+register(
+    id='nnlpDungeon',
+    entry_point='nlp2020.envs:nnlpDungeon',
+)
