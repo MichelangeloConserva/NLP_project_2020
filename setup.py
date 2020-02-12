8893817bb18268fcf312b866672fbb4c8d9d6176
Michelangelo Conserva <michelangelo.conserva@protonmail.com>
2020-02-12 14:38:02 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(name='nlp2020',
       version='0.0.1',
-      install_requires=['gym']  # And any other dependencies foo needs
+      install_requires=['gym'],
       packages=find_packages()
 )
 
