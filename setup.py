9deb38f0f8dcc61caadf4b0b37ba067347be6925
Michelangelo Conserva <michelangelo.conserva@protonmail.com>
2020-02-26 08:48:30 +0000

    Update setup.py with transformes requirement

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(name='nlp2020',
       version='0.0.1',
-      install_requires=['gym','tqdm'],
+      install_requires=['gym','tqdm','transformers'],
       packages=find_packages()
 )
 
