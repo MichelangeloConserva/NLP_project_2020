d5142827cc00d55dee0a99c973cbb7a7ed5e314e
Michelangelo Conserva <michelangelo.conserva@protonmail.com>
2020-03-06 15:06:24 +0000

    implementing hash and eq

--- nlp2020/agents/base_agent.py
+++ nlp2020/agents/base_agent.py
@@ -70,3 +70,11 @@ class BaseAgent:
     def __str__(self):               return self.name    
     def __repr__(self):              return self.name
     def __hash__(self):              return hash(self.name)
+    def __eq__(self, other):         return self.name == other.name
+    def __ne__(self, other):         return not(self == other)  
+         
+    
+    
+    
+    
+    
